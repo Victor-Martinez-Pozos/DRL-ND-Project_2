dcefe84694678e68b4c9be13b522b93ae0643ad6
Victor <vmartinez@machina.ai>
2020-11-17 17:44:49 -0600

    refactor: modularization of the sac base code

---
+++ sac/utils/utils.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def plot(frame_idx, rewards):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+    plt.plot(rewards)
+    plt.show()
